c54b9a219da7c52ad9e6dd1bcb17e39724806577
AndreasGassmann <andreas@andreasgassmann.ch>
2020-03-28 19:52:48 +0100

    feat(app): add google-services.json

--- android/app/src/main/java/com/contacttracer/app/MainActivity.java
+++ android/app/src/main/java/com/contacttracer/app/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
+      add(FCMPlugin.class);
     }});
   }
 }
