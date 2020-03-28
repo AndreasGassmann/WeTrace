b88828b7c7436181152beb1d8e4c76d30a8df3cb
AndreasGassmann <andreas@andreasgassmann.ch>
2020-03-28 21:51:41 +0100

    feat(app): use BLETracerPlugin

--- src/app/services/device-proximity.service.ts
+++ src/app/services/device-proximity.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
 import { StorageService } from './storage.service';
+import { Plugins, Capacitor } from '@capacitor/core';
+const { BLETracerPlugin } = Plugins;
+
+class BLETracerPluginMock {
+  static getCloseContacts() {
+    const generateDevice = (): BluetoothProximityUpdate => {
+      return {
+        deviceId: Math.random().toString(),
+        firstEncountered: Math.random().toString(),
+        lastEncountered: Math.random().toString()
+      };
+    };
+
+    const devices: BluetoothProximityUpdate[] = [];
+    const time = Math.floor(Math.random() * 5);
+
+    for (let index = 0; index < time; index++) {
+      devices.push(generateDevice());
+    }
+    return devices;
+  }
+}
 
 interface BluetoothProximityUpdate {
   deviceId: string;
-  approxDistance: number;
-  publicKey: string;
+  firstEncountered: string;
+  lastEncountered: string;
 }
 
 @Injectable({
@@ -14,23 +36,18 @@ export class DeviceProximityService {
   public listeners: ((proximities: BluetoothProximityUpdate[]) => void)[] = [];
 
   constructor(public readonly storageService: StorageService) {
-    const generateDevice = () => {
-      return {
-        deviceId: 'asdf',
-        approxDistance: Math.random() * 5,
-        publicKey: 'asdf'
-      };
-    };
 
     setInterval(() => {
-      const devices = [];
-      const time = Math.floor(Math.random() * 5);
-
-      for (let index = 0; index < time; index++) {
-        devices.push(generateDevice());
+      let result;
+      if (Capacitor.isPluginAvailable('BLETracerPlugin')) {
+        result = BLETracerPlugin.getCloseContacts({ sinceTimestamp: new Date().getTime() - 5 * 60 * 1000 });
+        console.log('result', result);
+      } else {
+        result = BLETracerPluginMock.getCloseContacts();
+        console.log('mock result', result);
       }
 
-      this.proximityUpdate(devices);
+      this.proximityUpdate(result);
     }, 2000);
   }
 
