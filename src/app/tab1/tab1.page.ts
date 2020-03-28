1257e94ce5417cde30934e66f499b9c49063bf0c
AndreasGassmann <andreas@andreasgassmann.ch>
2020-03-28 21:34:40 +0100

    feat(home): add alerts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DeviceProximityService } from '../services/device-proximity.service';
 import { PushService } from '../services/push.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -11,7 +12,9 @@ export class Tab1Page {
   status = 'bg-warning';
   numberOfClosePeople = 0;
 
-  constructor(private readonly deviceProximityService: DeviceProximityService, private readonly pushService: PushService) {
+  constructor(private readonly deviceProximityService: DeviceProximityService,
+              private readonly pushService: PushService,
+              private readonly alertController: AlertController) {
     this.deviceProximityService.listeners.push((proximities) => {
       console.log('proximities', proximities);
       this.numberOfClosePeople = proximities.length;
@@ -24,4 +27,51 @@ export class Tab1Page {
     });
   }
 
+  public async showInfectedAlert() {
+    const alert = await this.alertController.create({
+      header: 'You have been tested positive!',
+      message: 'By confirming you agree that an anonymous message about your result will be sent to our server.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    return alert.present();
+  }
+
+  public async showIHaveSymptomsAlert() {
+    const alert = await this.alertController.create({
+      header: 'You are showing symptoms!',
+      message: 'Your status will switch yellow, which means you shouldn\'t go out anymore.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Yes',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    return alert.present();
+  }
 }
