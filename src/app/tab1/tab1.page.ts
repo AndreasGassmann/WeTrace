e0363c6001d6cc6f756e159e6cb3b1c7362f6a7f
AndreasGassmann <andreas@andreasgassmann.ch>
2020-03-29 01:28:31 +0100

    feat(home): update text and report infections

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { DeviceProximityService } from '../services/device-proximity.service';
 import { PushService } from '../services/push.service';
 import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+
+enum Status {
+  HEALTHY = 'healthy',
+  POTENTIALLY_INFECTED = 'symptoms',
+  INFECTED = 'infected'
+}
 
 @Component({
   selector: 'app-tab1',
@@ -9,25 +16,70 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  status = 'bg-warning';
+  status: Status = Status.HEALTHY;
+  statusClass = 'bg-success';
+  statusTitle = 'Negative';
+  statusDescription = 'test';
   numberOfClosePeople = 0;
 
   constructor(private readonly deviceProximityService: DeviceProximityService,
-              private readonly pushService: PushService,
-              private readonly alertController: AlertController) {
+    private readonly pushService: PushService,
+    private readonly alertController: AlertController,
+    private readonly http: HttpClient) {
     this.deviceProximityService.listeners.push((proximities) => {
       console.log('proximities', proximities);
       this.numberOfClosePeople = proximities.length;
 
     });
 
+    this.setStatus(Status.INFECTED);
+
     this.pushService.listeners.push((infectedPeople) => {
       console.log('infectedPeople', infectedPeople);
-      this.status = Math.random() > 0.5 ? 'bg-success' : Math.random() > 0.5 ? 'bg-warning' : 'bg-danger';
+      Math.random() > 0.5 ?
+        this.setStatus(Status.HEALTHY) :
+        Math.random() > 0.5 ?
+          this.setStatus(Status.POTENTIALLY_INFECTED) :
+          this.setStatus(Status.INFECTED);
     });
   }
 
-  public async showInfectedAlert() {
+  public async setStatus(status: Status) {
+    switch (status) {
+      case Status.HEALTHY:
+        this.statusClass = 'bg-success';
+        this.statusTitle = `You're healthy! 😊`;
+        this.statusDescription = 'You have not had any close contact with a confirmed Covid-19 case.';
+        // Button 1 = I tested positive
+
+        break;
+
+      case Status.POTENTIALLY_INFECTED:
+        this.statusClass = 'bg-warning';
+        this.statusTitle = `You're potentially infected with Covid-19`;
+        this.statusDescription =
+          `You've had a close contact with a confirmed Covid-19 case.<br />Please monitor your symptoms and self-isolate for <strong>x more days</strong>. After this period, you will be considered healthy again.`;
+        // Add countdown
+        // Button 1 = I tested positive
+        // Button 2 = I tested negative
+
+        break;
+
+      case Status.INFECTED:
+        this.statusClass = 'bg-danger';
+        this.statusTitle = `You are a confirmed Covid-19 case`;
+        this.statusDescription = `We wish you a speedy recovery. Please self isolate to protect your loved ones and others.`;
+        /// Button 1 = I feel great again (no symptoms for 48 hours)
+
+        break;
+
+      default:
+
+        break;
+    }
+  }
+
+  public async showIAmInfectedAlert() {
     const alert = await this.alertController.create({
       header: 'You have been tested positive!',
       message: 'By confirming you agree that an anonymous message about your result will be sent to our server.',
@@ -42,7 +94,10 @@ export class Tab1Page {
         }, {
           text: 'Yes',
           handler: () => {
-            console.log('Confirm Okay');
+            this.http.post('https://contacttracer.dev.gke.papers.tech/api/v1/reports/', {
+              signature: Math.random().toString(),
+              severity: 2
+            }).subscribe(res => console.log(res));
           }
         }
       ]
